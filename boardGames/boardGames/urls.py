1b68509c77d5de77335f7f4d5cf7a3c0140d3a6a
PiekarzKarolina <piekarz.karolina@gmail.com>
2019-03-23 14:04:21 +0100

    rest testing

--- boardGames/boardGames/urls.py
+++ boardGames/boardGames/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from rest_framework import routers
+from boardGames.rental import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('rental/', include('rental.urls')),
     path('admin/', admin.site.urls),
 ]
