c460c62eca29569af4fe15fd61d7409c71a00abf
PiekarzKarolina <piekarz.karolina@gmail.com>
2019-03-23 11:53:25 +0100

    installed rental

--- boardGames/boardGames/settings.py
+++ boardGames/boardGames/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rental.apps.RentalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
